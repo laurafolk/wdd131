1d28499dda5ea688d1d4c40642eb462f9bf02a1e
laurafolk <laurafolk@yahoo.com>
2024-05-06 00:23:07 -0500

    Worked on Temple Album.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,10 +5,8 @@ var currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
 
 // Get the last modified date of the document
-// var lastModified = new Date(document.lastModified);
+var lastModified = new Date(document.lastModified);
 
 // Set the last modified date in the HTML
-function getLastModified() {
-    return new Date(document.lastModified);
-}
+
 var lastModified = getLastModified();
