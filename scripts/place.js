842afaefb05b24dca480338d33ebc6cfc4a994c8
laurafolk <laurafolk@yahoo.com>
2024-05-21 22:12:44 -0500

    Worked on and forgot to push.

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,6 @@
+const tempCelsius = 11
+const windSpeedKmh = 8
+
 function windChillFactorCelsius(tempCelsius, windSpeedKmh) {
     if (tempCelsius > 10.0 || windSpeedKmh <= 4.8) {
         return tempCelsius; // No wind chill effect
@@ -5,3 +8,15 @@ function windChillFactorCelsius(tempCelsius, windSpeedKmh) {
         return 13.12 + 0.6215 * tempCelsius - 11.37 * Math.pow(windSpeedKmh, 0.16) + 0.3965 * tempCelsius * Math.pow(windSpeedKmh, 0.16);
     }
 }
+// Below is an example of windChillFactor
+// const computeWindChill = function(windSpeed, temperature) {
+//     // Fahrenheit Only
+//     return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+//   };
+
+const windChill = windChillFactorCelsius(tempCelsius, windSpeedKmh);
+document.querySelector("#wind-chill-factor").textContent = windChill;
+
+var windChillFactor = windChillFactorCelsius(10, 20);
+
+console.log("Wind Chill Factor" + windChillFactor);
