abe4301f3865ebbd80748beb01b5dd8961ababef
laurafolk <laurafolk@yahoo.com>
2024-05-28 13:00:28 -0500

    Week05 Product Review Form.

--- scripts/form.js
+++ scripts/form.js
@@ -1,28 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const products = [
-        { id: 'fc-1888', name: "flux capacitor", avg_rating: 4.5 },
-        { id: 'fc-2050', name: "power laces", avg_rating: 4.7 },
-        { id: 'fs-1987', name: "time circuits", avg_rating: 3.5 },
-        { id: 'ac-2000', name: "low voltage reactor", avg_rating: 3.9 },
-        { id: 'jj-1969', name: "warp equalizer", avg_rating: 5.0 }
-    ];
-
-    const productSelect = document.getElementById('productN');
-
-    products.forEach(product => {
-        const option = document.createElement('option');
-        option.value = product.id;
-        option.textContent = product.name;
-        productSelect.appendChild(option);
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5,
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7,
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5,
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9,
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0,
+    },
+  ];
+  
+  function createOptions(array) {
+    const templateOptions = array.map((item) => {
+      return `<option value="${item.id}">${item.name}</option>`;
     });
-
-    if (localStorage.getItem('reviewCount') === null) {
-        localStorage.setItem('reviewCount', '0');
+  
+    const selectElement = document.querySelector("#productN");
+    selectElement.innerHTML += templateOptions;
+  }
+  
+  const form = document.querySelector(".prf");
+  const numVisits = document.querySelector(".numVisits");
+  let getVisits = parseInt(localStorage.getItem("numReviews")) || 0;
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const pathname = window.location.pathname;
+  
+    if (pathname.includes("form.html")) {
+      createOptions(products);
+    } else if (pathname.includes("review.html")) {
+      getVisits += 1;
+      localStorage.setItem("numReviews", getVisits);
+      numVisits.textContent = getVisits;
     }
-
-    document.querySelector('form').addEventListener('submit', function() {
-        let reviewCount = parseInt(localStorage.getItem('reviewCount'), 10);
-        reviewCount += 1;
-        localStorage.setItem('reviewCount', reviewCount.toString());
-    });
-});
+  });
