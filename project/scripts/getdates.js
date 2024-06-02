822bd42d7790449aa0a7129930e05c087607b1a8
laurafolk <laurafolk@yahoo.com>
2024-06-02 00:31:52 -0500

    Week06 Final Project

---
+++ project/scripts/getdates.js
@@ -0,0 +1,11 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Set the current year in the HTML
+document.getElementById("currentYear").textContent = currentYear;
+
+// Get the last modified date of the document
+var lastModified = new Date(document.lastModified);
+
+// Set the last modified date in the HTML
+document.getElementById("lastmodified").innerHTML = lastModified
---
+++ project/scripts/laurascc.js
@@ -0,0 +1,28 @@
+//select HTML elements and assign to constants
+const hamButton = document.querySelector('#mobile-menu-button');
+const navigation = document.querySelector('nav');
+const page = document.querySelector('main');
+const heading = document.querySelector('#header-title');
+const bottom = document.querySelector('footer');
+
+//create event listener for the hamburger menu button
+//toggle the open class on/off for the nav menu and button, when the button is clicked
+hamButton.addEventListener('click', () => {
+    hamButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
+
+page.addEventListener('click', () => {
+    hamButton.classList.remove('open');
+    navigation.classList.remove('open');
+});
+
+heading.addEventListener('click', () => {
+    hamButton.classList.remove('open');
+    navigation.classList.remove('open');
+});
+
+bottom.addEventListener('click', () => {
+    hamButton.classList.remove('open');
+    navigation.classList.remove('open');
+});
